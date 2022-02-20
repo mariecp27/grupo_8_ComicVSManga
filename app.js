56e08b743b4c9426551bf4323ac0923a3c4b3e9e
Erixk90 <erickleonesreyes@gmail.com>
2022-02-20 15:21:02 -0500

    Carpetas del carrito

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ app.use(express.static(publicPath));
 app.listen(3030, () => {console.log("Servidor corriendo en el puerto 3030, ¡vamos al Manga!")});
 
 app.get('/', (req,res) => {res.sendFile(path.join(__dirname, '/views/index.html'))});
+
+app.get('/login', (req,res) => {res.sendFile(path.join(__dirname, '/views/login.html'))});
+
+app.get('/tienda', (req,res) => {res.sendFile(path.join(__dirname, '/views/productDetails.html'))});
+
+app.get('/Carrito', (req,res) => {res.sendFile(path.join(__dirname, '/views/productCart.html'))});
+
+app.get('/iniciarSesion', (req,res) => {res.sendFile(path.join(__dirname, '/views/register.html'))});
